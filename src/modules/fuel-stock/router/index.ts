f06175d1ce7f8d2461dcb517f9f23c0e9ef89085
vysingsun <vysingsun@gmail.com>
2024-10-05 11:53:47 +0700

    create fuelStock

---
+++ src/modules/fuel-stock/router/index.ts
@@ -0,0 +1,32 @@
+const MODULE_NAME = 'Fuel-stock'
+const MODULE_PATH = '/fuel-stock'
+
+export const routes = [
+    {
+        module: MODULE_NAME,
+        name: 'list',
+        path: MODULE_PATH,
+        meta: {
+            title: MODULE_NAME,
+        },
+        component: () => import('@/modules/fuel-stock/views/index.vue'),
+    },
+]
+
+const FUEL_STOCK_ROUTES = [
+    {
+        children: routes,
+        initModule: MODULE_NAME.toLowerCase(),
+        name: MODULE_NAME,
+        path: MODULE_PATH,
+        meta: {
+            title: MODULE_NAME,
+            moduleName: MODULE_NAME,
+        },
+        beforeEnter: async (to: any, from: any, next: any) => {
+            next()
+        },
+    },
+]
+
+export const FUEL_STOCK = { FUEL_STOCK_ROUTES, MODULE_NAME, MODULE_PATH }
---
+++ src/modules/fuel-stock/store/index.ts
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { fuel_stockService } from '../services/api.service'
+interface Ifuel {
+    _id: any
+    fuel_name: string
+    station_id: any
+    coefficient_id: any
+    createdAt: any
+}
+class FormData {
+    _id: any
+    fuel_id?: Ifuel
+    quantity_ton?: number | null
+    quantity_liter?: number | null
+    amount_ton?: number | null
+    amount_liter_us?: number | null
+    amount_liter_khr?: number | null
+    supplier_name?: string
+    exchange_rate?: number | null
+    total_amount_us?: number | null
+    station_id: any
+    createdAt: any
+}
+
+export const useFuelStockStore = defineStore('useFuelStockStore', () => {
+    const formData = ref(new FormData())
+    const saveFuelStock = async () => {
+        console.log(formData.value, 'pay')
+        await fuel_stockService.create(formData.value)
+    }
+    return {
+        formData,
+        saveFuelStock,
+    }
+})
