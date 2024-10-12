25d9dfbf24f27156bcaa3773892dc05bfacfe994
vysingsun <vysingsun@gmail.com>
2024-10-12 13:45:18 +0700

    fix: ⛩ useCookie

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,9 @@ import { initFlowbite } from 'flowbite'
 
 import BaseLoading from './components/app/BaseLoading.vue'
 import ShapeBgAnimate from './components/app/ShapeBgAnimate.vue'
-import { getFromCache, removeCaches } from '@/composables/useCache'
+import { removeCaches } from '@/composables/useCache'
 import BaseModal from '@/components/app/BaseModal.vue'
-const getToken = () => {
-    const token = getFromCache('token')?.value
 
-    return token && `Bearer ${token}`
-}
 // Axios default config
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
@@ -25,10 +21,6 @@ axios.interceptors.request.use(
     function (req) {
         if (req.url === 'login') isUnauthorized.value = false
         if (isUnauthorized.value) throw new Error('Unauthorized access detected. Further requests blocked.')
-
-        const token = req.headers.has('Authorization') ? req.headers.Authorization : getToken()
-
-        req.headers['Authorization'] = token
 
         return req
     },
--- src/modules/auth/services/api.service.ts
+++ src/modules/auth/services/api.service.ts
@@ -1,24 +1,10 @@
 import axios from 'axios'
-import { getFromCache } from '@/composables/useCache'
-
-const getToken = () => {
-    const token = getFromCache('token')?.value
-    return token && `Bearer ${token}`
-}
 
 export const authService = {
     login: async (payload: object) => {
         return axios.post('auth/login', payload, { withCredentials: true })
     },
     logout: async () => {
-        return axios.post(
-            'auth/logout',
-            {},
-            {
-                headers: {
-                    Authorization: getToken(),
-                },
-            },
-        )
+        return axios.post('auth/logout')
     },
 }
