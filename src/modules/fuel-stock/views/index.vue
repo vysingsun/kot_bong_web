fc2bb9933af506ef39869b8788cbc3a72e4a3f90
Vysing Sun <vysing.sun@wingbank.com.kh>
2024-10-18 11:51:33 +0700

    fix: 🧨 width loader & key tb load

--- src/modules/fuel-stock/views/index.vue
+++ src/modules/fuel-stock/views/index.vue
@@ -1,5 +1,6 @@
 <template>
     <TablePaging
+        :key="table_key"
         name="Fuel Income"
         :headers="store.headers"
         column-no
@@ -15,10 +16,14 @@
     import { fuel_stockService } from '@/modules/fuel-stock/services/api.service'
     import { useFuelStockStore } from '@/modules/fuel-stock/store/index'
 
+    const table_key = ref(0)
     const store = useFuelStockStore()
     onMounted(() => {
         initFlowbite()
     })
+    const load = () => {
+        table_key.value += 1
+    }
 </script>
 
 <style lang="scss" scoped></style>
