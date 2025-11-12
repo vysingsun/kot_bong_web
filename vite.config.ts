51a8f412891f4228c627f18e7f953058fdd8e263
vysingsun <vysingsun@gmail.com>
2025-11-12 19:28:04 +0700

    fix: 🚄 enable create stock & sold ft date

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -15,12 +14,7 @@ export default defineConfig({
             },
         },
     },
-    plugins: [
-        vue(),
-        AutoImport({
-            imports: ['vue-i18n'],
-        }),
-    ],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
