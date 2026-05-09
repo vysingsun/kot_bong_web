75e7f3bff68de2a034631333187bea688590da9e
vysingsun <vysingsun@gmail.com>
2026-05-09 22:46:59 +0700

    fix: 💦 normal / for dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/app/',
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://kot-bong-api-dev.onrender.com',
-                changeOrigin: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    console.log(env.VITE_APP_MODE, 'mode')
+
+    return {
+        base: env.VITE_APP_MODE === 'development' ? '/' : '/app/',
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'https://kot-bong-api-dev.onrender.com',
+                    changeOrigin: true,
+                },
             },
         },
-    },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
+    }
 })
